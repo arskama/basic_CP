133ff47f453c880088a7ba639fe951da816ce77e
Arnaud Mandy <arnaud.mandy@intel.com>
2023-01-25 16:18:15 +0200

    new starting point
    Signed-off-by: Arnaud Mandy <arnaud.mandy@intel.com>

---
+++ d_worker.js
@@ -0,0 +1,35 @@
+
+/*
+var n = 1;
+search: while (true) {
+  n += 1;
+  for (var i = 2; i <= Math.sqrt(n); i += 1)
+    if (n % i == 0)
+     continue search;
+  // found a prime!
+  postMessage(n);
+}
+*/
+
+/*let n = 0;
+let timer = setInterval(function() {
+  n = n + 1;
+  postMessage(n);
+}, 1000);
+*/
+function pressureCallbackWkr(update) {
+    console.log("Update from Worker");
+    console.log("cpuState X = " + update[0].state);
+    console.log("cpu factor X = " + update[0].factors[0]);
+  if (update[0].factors[1])
+    console.log("cpu factor X = " + update[0].factors[1]);
+    console.log("timestamp = " + update[0].time);
+    postMessage(update[0].toJSON());
+};
+
+let observerWorker = new PressureObserver(pressureCallbackWkr, {sampleRate : 0.5});
+
+observerWorker.observe("cpu");
+
+console.log("started from worker");
+
