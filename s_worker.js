e90963c2e283b534620574b9f19708bc8e5325cd
Arnaud Mandy <arnaud.mandy@intel.com>
2023-01-27 16:40:46 +0200

    Compute Pressure: starting observer from shared worker.
    Signed-off-by: Arnaud Mandy <arnaud.mandy@intel.com>

--- s_worker.js
+++ s_worker.js
@@ -1,27 +1,30 @@
 onconnect = function(e) {
   var port = e.ports[0]; //get the port
   console.log("in onconnect------------>");
+  let started = false;
 
-//  port.start(); // Open the port connection to enable two-way communication
+  port.start(); // Open the port connection to enable two-way communication
+
+  let observerWorker = new PressureObserver(pressureCallbackWkr, {sampleRate : 0.5});
+  console.log ("observer is " + typeof observerWorker)
+
   port.onmessage = function(e) {
     console.log("received from client. " + e.data[0]);
+    if (started === false)
+      observerWorker.observe("cpu");
+    started = true;
     port.postMessage(e.data[0]);
   }
-}
-/*
-function pressureCallbackWkr(update) {
+  function pressureCallbackWkr(update) {
     console.log("Update from Worker");
     console.log("cpuState X = " + update[0].state);
     console.log("cpu factor X = " + update[0].factors[0]);
-  if (update[0].factors[1])
-    console.log("cpu factor X = " + update[0].factors[1]);
+    if (update[0].factors[1])
+      console.log("cpu factor X = " + update[0].factors[1]);
     console.log("timestamp = " + update[0].time);
-    postMessage(update[0].toJSON());
+    port.postMessage(update[0].toJSON());
 };
 
-let observerWorker = new PressureObserver(pressureCallbackWkr, {sampleRate : 0.5});
-
-observerWorker.observe("cpu");
+}
 
 console.log("started from worker");
-*/
